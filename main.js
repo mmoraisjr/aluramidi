8ea4609fc07b85aa860763c9b67bb2313d4ab871
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-05 21:01:32 -0300

    finished project

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+  elemento = document.querySelector(idElementoAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    console.log("Elemento nao encontrado");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -7,10 +13,20 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 for (let cont = 0; cont < listaDeTeclas.length; cont++) {
   const tecla = listaDeTeclas[cont];
   const obtemClass = tecla.classList[1];
-//   const idAudio = "#som_" + obtemClass;
-  const idAudio = `#som_${obtemClass}`; 
+  //   const idAudio = "#som_" + obtemClass;
+  const idAudio = `#som_${obtemClass}`;
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
+
+  tecla.onkeydown = function (e) {
+    if (e.code === 'Space' || e.code === 'Enter') {
+      tecla.classList.add("ativa");
+    }
+  }
+  
+  tecla.onkeyup = function (e) {
+      tecla.classList.remove("ativa");
+  }
 }
